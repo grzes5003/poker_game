a2f0e665fa07d1f45a1cef6a8e8b89db1c17d2ed
grzes5003 <grzes.kulinski@gmail.com>
2015-11-14 16:30:37 +0100

    rozwiniete klasy, powoli dodawane metody

--- poker_game/poker_game/card.h
+++ poker_game/poker_game/card.h
@@ -1,13 +1,17 @@
 #pragma once
+#include <utility>
+#include <iostream>
 
 class Card
 {
+	bool isVisible;
 	enum Kolor{Pik,Kier,Trefl,Karo};
 	enum Figura { As = 0, Dwa = 2, Trzy = 3, Cztery = 4, Piec = 5, Szesc = 6, Siedem = 7, Osiem = 8, Dziewiec = 9, Dziesiec = 10, Walet = 11, Dama = 12, Krol = 13 };
 	const Kolor kolor;
 	const Figura figura;
 public:
 	Card(Kolor _kolor,Figura _figura);
+	std::pair<Kolor, Figura> wartosc();
 	virtual ~Card();
 };
 
--- poker_game/poker_game/table.h
+++ poker_game/poker_game/table.h
@@ -1,6 +1,17 @@
 #pragma once
+#include "card.h"
+#include "player.h"
+#include <vector>
+
 class Table
 {
+protected:
+	struct player_cards{
+		Card card1;
+		Card card2;
+	};
+	std::vector<Card> cardsOnTable;
+	std::vector<Player> playersInGame;
 public:
 	Table();
 	virtual ~Table();
