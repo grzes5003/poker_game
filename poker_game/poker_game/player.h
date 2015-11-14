93dd570fae66c64eed66b96b0a49d6ce7c5efb43
grzes5003 <grzes.kulinski@gmail.com>
2015-11-14 19:27:56 +0100

    dobry

--- poker_game/poker_game/player.h
+++ poker_game/poker_game/player.h
@@ -1,11 +1,11 @@
 #pragma once
 #include <vector>
-#include "card.h"
+#include "table.h"
 
 class Player
 {
 public:
-	int odzywka(); //-1=pass,0=call,jakas kwota=raise;
+	int odzywka( Table stol ); //-1=pass,0=call,jakas kwota=raise;
 	void zobaczKarty(); //gracz daje reqesta do managera, a ten uzywa tej funkcji by zobaczyc karty;
 	Player();
 	virtual ~Player();
