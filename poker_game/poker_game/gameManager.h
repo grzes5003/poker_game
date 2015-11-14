df37eb71a23ad045d9485256bea9ce79f97a777b
grzes5003 <grzes.kulinski@gmail.com>
2015-11-14 18:47:55 +0100

    nowy commit

--- poker_game/poker_game/gameManager.h
+++ poker_game/poker_game/gameManager.h
@@ -1,10 +1,16 @@
 #pragma once
-
+#include "table.h"
+#include <array>
 
 class GameManager
 {
+
+	int begginer_index; // index typa ktory ma zaczynac dana licytacje;
+	std::vector<Player, std::vector<int, std::array<Card, 2>>> playerlist; //gracz, hajs, jego karty;
 public:
-	GameManager();
+	GameManager( std::vector<Player,std::vector<int,std::array<Card,2>>> _playerList );
+	void licytacja();
+
 	virtual ~GameManager();
 };
 
--- poker_game/poker_game/player.h
+++ poker_game/poker_game/player.h
@@ -1,10 +1,11 @@
 #pragma once
-#include "table.h"
+#include <vector>
+#include "card.h"
 
 class Player
 {
 public:
-	int odzywka(Table stol); //-1=pass,0=call,jakas kwota=raise;
+	int odzywka(); //-1=pass,0=call,jakas kwota=raise;
 	void zobaczKarty(); //gracz daje reqesta do managera, a ten uzywa tej funkcji by zobaczyc karty;
 	Player();
 	virtual ~Player();
